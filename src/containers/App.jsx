460e23e29f2bb87381ba542957dfc73c25b882cf
Julio Gragirena <juliogragirena@hotmail.com>
2020-05-24 23:33:59 -0400

    Agregado lista de videos y crear archivo js (useInitialState.js)

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,42 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer';
+import useInitialState from '../Hooks/useInitialState';
 import '../asset/styles/App.scss';
 
-const App = () => (
-  <div className='App'>
-    <Header />
-    <Search />
+const API = 'http://localhost:3000/initalState/';
+const App = () => {
+  const initalState = useInitialState(API);
+  return (
+    <div className='App'>
+      <Header />
+      <Search />
+      {initalState.mylist !== undefined && initalState.mylist.length > 0 && (
+        <Categories title='Mi Lista'>
+          <Carousel>
+            {
+              // eslint-disable-next-line react/jsx-props-no-spreading
+              initalState.mylist.map((item) => (<CarouselItem key={item.id} {...item} />))
+            }
+            <CarouselItem />
+          </Carousel>
+        </Categories>
+      )}
 
-    <Categories title='Mi Lista'>
-      <Carousel>
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-      </Carousel>
-    </Categories>
+      <Categories title='Tendencias'>
+        <Carousel>
+          {
+            // eslint-disable-next-line react/jsx-props-no-spreading
+            initalState.trends.map((item) => (<CarouselItem key={item.id} {...item} />))
+          }
+          <CarouselItem />
+        </Carousel>
+      </Categories>
 
-    <Categories title='Tendencias'>
-      <Carousel>
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-      </Carousel>
-    </Categories>
-
-    <Categories title='Orginales'>
-      <Carousel>
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-      </Carousel>
-    </Categories>
-    <Footer />
-  </div>
-);
+      <Categories title='Orginales'>
+        <Carousel>
+          {
+            // eslint-disable-next-line react/jsx-props-no-spreading
+            initalState.originals.map((item) => (<CarouselItem key={item.id} {...item} />))
+          }
+          <CarouselItem />
+        </Carousel>
+      </Categories>
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
